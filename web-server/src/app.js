bd9c31be8595bbd445307aaa48a6c809e30aa2b3
Fernando Serrano <fernandos@eventbrite.com>
2020-05-09 12:06:10 -0300

    Able to configue server listening port from outside

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -13,97 +13,109 @@ const publicAppDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// app.set sets express settings in the form of key-value pairs
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
-
-// Setup static directory to serve
-app.use(express.static(publicAppDirectoryPath))
-
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather app',
-        name: 'Fernando Serrano',
-    })
-})
-
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'Weather app - about',
-        body: 'Some dinamic content',
-        name: 'Fernando Serrano',
-    })
-})
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Weather app - help',
-        body: 'Some mode dinamic content',
-        name: 'Fernando Serrano',
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: 'Weather app - help',
-        body: 'Help article not found',
-        name: 'Fernando Serrano',
-    })
-})
-
-app.get('/weather', (req, res) => {
-    if(! req.query.address) {
-        return res.send({
-            error: 'Address must be provided'
+const appInit = (app, port = 80) => {
+    
+    // app.set sets express settings in the form of key-value pairs
+    app.set('view engine', 'hbs')
+    app.set('views', viewsPath)
+    hbs.registerPartials(partialsPath)
+    
+    // Setup static directory to serve
+    app.use(express.static(publicAppDirectoryPath))
+    
+    app.get('', (req, res) => {
+        res.render('index', {
+            title: 'Weather app',
+            name: 'Fernando Serrano',
         })
-    }
-
-    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
-        if(error){
+    })
+    
+    
+    app.get('/about', (req, res) => {
+        res.render('about', {
+            title: 'Weather app - about',
+            body: 'Some dinamic content',
+            name: 'Fernando Serrano',
+        })
+    })
+    
+    app.get('/help', (req, res) => {
+        res.render('help', {
+            title: 'Weather app - help',
+            body: 'Some mode dinamic content',
+            name: 'Fernando Serrano',
+        })
+    })
+    
+    app.get('/help/*', (req, res) => {
+        res.render('404', {
+            title: 'Weather app - help',
+            body: 'Help article not found',
+            name: 'Fernando Serrano',
+        })
+    })
+    
+    app.get('/weather', (req, res) => {
+        if(! req.query.address) {
             return res.send({
-                error, 
-            })
-        } else {
-            forecast(latitude, longitude, (error, data) => {
-                if (error) {
-                    return res.send({ error })
-                }
-                res.send({
-                    forecast: data,
-                    location,
-                    address: req.query.address, 
-                })
+                error: 'Address must be provided'
             })
         }
-
-    })
-})
-
-app.get('/products', (req, res) => {
-    console.log(req.query)
-
-    if(!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
+    
+        geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+            if(error){
+                return res.send({
+                    error, 
+                })
+            } else {
+                forecast(latitude, longitude, (error, data) => {
+                    if (error) {
+                        return res.send({ error })
+                    }
+                    res.send({
+                        forecast: data,
+                        location,
+                        address: req.query.address, 
+                    })
+                })
+            }
+    
         })
-    }
-    
-    res.send({
-        products: []
     })
     
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: 'Page not found',
-        body: 'Page not found',
-        name: 'Fernando Serrano',
+    app.get('/products', (req, res) => {
+        console.log(req.query)
+    
+        if(!req.query.search) {
+            return res.send({
+                error: 'You must provide a search term'
+            })
+        }
+        
+        res.send({
+            products: []
+        })
+        
     })
-})
+    
+    app.get('*', (req, res) => {
+        res.render('404', {
+            title: 'Page not found',
+            body: 'Page not found',
+            name: 'Fernando Serrano',
+        })
+    })
+    
+    
+    app.listen(port, () => {
+        console.log('Server is up on port ' + port)
+    })
+}
 
-app.listen(port = 3000, () => {
-    console.log('Server is up on port ' + port)
-})
+let port = 80
+if (process.argv[2]) {
+    port = process.argv[2]
+}
+
+appInit(app, port)
+
